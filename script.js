c07c47c160fce7662ca4132e88a2908ff3f5258d
Divyasnehankitha03 <142600726+Divyasnehankitha03@users.noreply.github.com>
2023-09-06 19:35:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const celsiusEl = document.getElementById("celsius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
+
+function computeTemp(event) {
+  const currentValue = +event.target.value;     //adding + helps in converting string to value if string is passed
+
+  switch (event.target.name) {
+    case "celsius":
+      kelvinEl.value = (currentValue + 273.32).toFixed(2);     //.tofixed(2) is to limit the value upto 2 decimals
+      fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
+      break;
+    case "fahrenheit":
+      celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
+      kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+      break;
+    case "kelvin":
+      celsiusEl.value = (currentValue - 273.32).toFixed(2);
+      fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
+      break;
+    default:
+      break;
+  }
+}
